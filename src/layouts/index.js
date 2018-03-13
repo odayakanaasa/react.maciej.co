7f3799520d0864279048828312bbd011653be429
Odayaka <35795454+odayakanaasa@users.noreply.github.com>
2018-03-13 21:21:21 +0700

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import * as PropTypes from "prop-types"
 import Link from "gatsby-link"
 import '../css/main.scss'
-import '../css/variables.scss',
-import '../css/base.scss',
-import '../css/replica-regular.scss',
-import '../css/replica-bold.scss',
-import '../css/replica-mono.scss',
-import '../css/typography.scss',
-import '../css/navigation.scss',
-import '../css/hero.scss',
-import '../css/cards.scss',
-import '../css/credits.scss',
-import '../css/case.scss',
-import '../css/media.scss',
-import '../css/dark.scss',
+import '../css/variables.scss'
+import '../css/base.scss'
+import '../css/replica-regular.scss'
+import '../css/replica-bold.scss'
+import '../css/replica-mono.scss'
+import '../css/typography.scss'
+import '../css/navigation.scss'
+import '../css/hero.scss'
+import '../css/cards.scss'
+import '../css/credits.scss'
+import '../css/case.scss'
+import '../css/media.scss'
+import '../css/dark.scss'
 
 import './index.scss'
 
