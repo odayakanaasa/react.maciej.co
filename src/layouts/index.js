d4a9d09c9d26518158120f44d06240e52e967014
Odayaka <35795454+odayakanaasa@users.noreply.github.com>
2018-03-13 21:08:55 +0700

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as PropTypes from "prop-types"
 import Link from "gatsby-link"
 import '../css/main.scss'
-import 'index.scss'
+import './index.scss'
 
 export default class Template extends React.Component {
   static propTypes = {
